6f399b09ad61de3078de165fbc94da20aefbf335
Akash M <99746120+Akash7422@users.noreply.github.com>
2024-07-01 19:59:49 +0530

    Update enum.c

--- C_Programming/enum.c
+++ C_Programming/enum.c
@@ -4,6 +4,7 @@ int main(){
     //check pull request
     enum days {mon, tue, wed, thu, fri, sat, sun};
     enum days d1=mon, d2=tue;
+    //check for merge conflict
     printf("%d",d1);
     return 0;
 }
